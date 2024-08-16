8d3a9bb28e3f0e4ee8f175cf64a0793d362bda6f
Tiago Machado <tiago.machado.1987@gmail.com>
2024-08-16 23:43:04 +0200

    adding accordion

--- src/components/CardWorkerFile.jsx
+++ src/components/CardWorkerFile.jsx
@@ -3,35 +3,66 @@ import PropTypes from "prop-types";
 
 const CardWorkerFile = (props) => {
   return (
-    <div className="worker-infos">
-      <img
-        src="/src/assets/images/icons/worker-file.png"
-        alt="fiche d'artisan"
-      />
-      <ul>
-        <li>
-          <strong>Nom :</strong> {props.name}
-        </li>
-        <li>
-          <strong>Métier :</strong> {props.specialty}
-        </li>
-        <li>
-          <strong>Note :</strong> {printStars(props.note)} {props.note} / 5
-        </li>
-        <li>
-          <strong>Ville :</strong> {props.location}
-        </li>
-        <li>
-          <strong>Email :</strong> {props.email}
-        </li>
-        <li>
-          <strong>Catergorie :</strong> {props.category}
-        </li>
-        <li>
-          <strong>Site :</strong> {props.website}
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="worker-infos">
+        <img
+          src="/src/assets/images/icons/worker-file.png"
+          alt="fiche d'artisan"
+        />
+        <ul>
+          <li>
+            <strong>Nom :</strong> {props.name}
+          </li>
+          <li>
+            <strong>Métier :</strong> {props.specialty}
+          </li>
+          <li>
+            <strong>Note :</strong> {printStars(props.note)} {props.note} / 5
+          </li>
+          <li>
+            <strong>Ville :</strong> {props.location}
+          </li>
+          <li>
+            <strong>Email :</strong> {props.email}
+          </li>
+          <li>
+            <strong>Catergorie :</strong> {props.category}
+          </li>
+          <li>
+            <strong>Site :</strong> {props.website}
+          </li>
+        </ul>
+      </div>
+
+      <div
+        className="accordion rounded mb-5"
+        id="accordionPanelsStayOpenExample"
+      >
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseOne"
+              aria-expanded="true"
+              aria-controls="panelsStayOpen-collapseOne"
+            >
+              À propos
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="panelsStayOpen-headingOne"
+          >
+            <div className="accordion-body">
+              {props.about}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -43,6 +74,7 @@ CardWorkerFile.propTypes = {
   email: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
   website: PropTypes.string.isRequired,
+  about: PropTypes.string.isRequired,
 };
 
 export default CardWorkerFile;
