4bd88d21c7fe8f969221365d1a0341f6c512aafb
Tiago Machado <tiago.machado.1987@gmail.com>
2024-08-19 22:56:34 +0200

    add final considerations in working ambiance

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/datas.json',
-          dest: 'assets'
-        },
-        // {
-        //   src: '404.html',
-        //   dest: ''
-        // }
-      ]
-    })
-  ],
-  base: '/trouve_ton_artisan/',
-})
+export default defineConfig(({ mode }) => {
+
+  const base = mode === 'production' ? '/trouve_ton_artisan/' : '/';
+
+  return {
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'src/assets/datas.json',
+            dest: 'assets'
+          }
+        ]
+      })
+    ],
+    base: base,
+  };
+});
